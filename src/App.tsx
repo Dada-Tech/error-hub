7131416eb821281cb4de3ae54019af4d3ed1a117
davir <ddebrah@ymail.com>
2023-07-15 21:47:58 -0600

    Re-did routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import * as React from "react";
+import { createBrowserRouter } from "react-router-dom";
 import Login from './pages/Login/Login'
-import {GlobalStyle} from "./theme/theme";
+// import {GlobalStyle} from "./theme/theme";
 
-
-const App: React.FC = () => {
-  return (
-      <GlobalStyle>
-        <Routes>
-          {/*<Route path="/" element={Login}/>*/}
-            <Route path="/" Component={Login} />
-          {/*<Route path='/signup' element={Signup}></Route>*/}
-        </Routes>
-      </GlobalStyle>
-
-  );
-}
+const App = createBrowserRouter([
+    {
+        path: "/",
+        element: <Login />,
+    },
+]);
 
 export default App;
