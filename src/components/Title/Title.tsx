83a9c89cd966ee5aadb246c31c1e23e019e6396b
davir <ddebrah@ymail.com>
2023-07-17 16:36:29 -0600

    Title components created

---
+++ src/components/Title/Title.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Title.module.scss';
+
+function Title () {
+    return (
+            <div className={classes.title}></div>
+    );
+}
+
+export default Title;
