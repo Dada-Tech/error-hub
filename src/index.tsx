583c14cf5fa987342565deca4688628e3dcf8a5d
davir <ddebrah@ymail.com>
2023-07-20 19:25:21 -0600

    few tweaks

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from "react-router-dom";
 import './index.css';
+import './styles.module.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
