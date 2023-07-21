5ee1003b75525181e020c5a666c61e3048feaaf5
davir <ddebrah@ymail.com>
2023-07-20 19:22:12 -0600

    tailwind configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+// @import('tailwindcss').Config;
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{html,js,tsx}',
+    './components/**/*.{html,js,tsx}',
+    './node_modules/@my-company/tailwind-components/**/*.js',
+  ],
   theme: {
-    extend: {},
+    // fill: ({ theme }) => ({
+    //   Blue: theme('colors.blue')
+    // })
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+
+    colors: {
+      'blue': '#0089ED',
+      'white': '#FFFFFF',
+      'lightgray': '#8D8D8D',
+      'black': '#000000'
+    }
   },
-  plugins: [],
+  fontFamily: {
+    sans: ['Poppins', 'sans-serif'],
+    serif: ['Saira SemiCondensed','serif'],
+    // serif: ['Merriweather', 'serif'],
+  },
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
 }
 
