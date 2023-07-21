ad525412c5b98a907978d7c4dd22beaf4c0b150b
davir <ddebrah@ymail.com>
2023-07-20 20:08:57 -0600

    clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import * as React from "react";
 import { createBrowserRouter } from "react-router-dom";
-// import Login from './pages/Login/Login'
 import Home from "./pages/Home/Home";
-// import {GlobalStyle} from "./theme/theme";
 
 const App = createBrowserRouter([
     {
