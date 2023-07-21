f25a36f87fb7c2e16bb048ff22a3521711a7b453
davir <ddebrah@ymail.com>
2023-07-20 20:09:08 -0600

    clean-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-// @import('tailwindcss').Config;
 module.exports = {
   content: [
-    './pages/**/*.{html,js,tsx}',
-    './components/**/*.{html,js,tsx}',
-    './node_modules/@my-company/tailwind-components/**/*.js',
+    './src/**/*.{html,js,tsx}',
   ],
+
   theme: {
-    // fill: ({ theme }) => ({
-    //   Blue: theme('colors.blue')
-    // })
+
     screens: {
       sm: '480px',
       md: '768px',
@@ -21,14 +17,16 @@ module.exports = {
       'blue': '#0089ED',
       'white': '#FFFFFF',
       'lightgray': '#8D8D8D',
-      'black': '#000000'
+      'black': '#000000',
+      'secondBlack': '#00204A',
     }
   },
+
   fontFamily: {
     sans: ['Poppins', 'sans-serif'],
     serif: ['Saira SemiCondensed','serif'],
-    // serif: ['Merriweather', 'serif'],
   },
+
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
