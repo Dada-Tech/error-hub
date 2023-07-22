523ae94765cdec97ba132a7430325d04975aba28
davir <ddebrah@ymail.com>
2023-07-22 10:54:02 -0600

    minor clean-up

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
-import * as React from "react";
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import * as React from "react";
+import { screen } from '@testing-library/react';
+// import App from './App';
 
 test('renders learn react link', () => {
   // render(<App />);
