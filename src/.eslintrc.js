71d3b35f10367cdd0b7da0757c354f44c0fe2477
davir <ddebrah@ymail.com>
2023-07-22 10:53:49 -0600

    Plugins updated

--- src/.eslintrc.js
+++ src/.eslintrc.js
@@ -1,13 +1,16 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: ['@typescript-eslint'],
-    extends: [
-        'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
-    ],
-    rules: {
-        // Add specific ESLint rules if needed
-        "no-console": [0],
-    },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'jsx-a11y', 'react'],
+  extends: [
+    'airbnb',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+  ],
+  rules: {
+    // Add specific ESLint rules if needed
+    'no-console': [0],
+    'react/jsx-filename-extension': ['error', { extensions: ['.tsx', '.jsx'] }],
+  },
 };
