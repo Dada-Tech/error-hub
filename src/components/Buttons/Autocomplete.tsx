1ea25a99dbc546f0028a358b7012176d9e7cbf31
davir <ddebrah@ymail.com>
2023-07-22 10:55:31 -0600

    minor updates

--- src/components/Buttons/Autocomplete.tsx
+++ src/components/Buttons/Autocomplete.tsx
@@ -1,10 +1,10 @@
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-import React from "react";
+import React from 'react';
 
 const options = [
-    { label: 'The Godfather', id: 1 },
-    { label: 'Pulp Fiction', id: 2 },
+  { label: 'The Godfather', id: 1 },
+  { label: 'Pulp Fiction', id: 2 },
 ];
 
 export default options;
