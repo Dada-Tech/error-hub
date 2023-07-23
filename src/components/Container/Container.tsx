b36eee90c68c5714161b9bd386f9493d361ade31
davir <ddebrah@ymail.com>
2023-07-22 22:41:57 -0600

    A container with a white background for the home-page.

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Box } from '@mui/material';
+
+function Container() {
+  return (
+    <Box
+      sx={{
+        width: '1744px',
+        height: '582px',
+        backgroundColor: '#ffffff', // White background color
+        position: 'fixed',
+        top: '422px',
+        bottom: '0',
+        left: '0',
+        right: '0',
+        mx: 'auto',
+      }}
+    >
+      {/* Content of the container goes here */}
+    </Box>
+  );
+}
+
+export default Container;
