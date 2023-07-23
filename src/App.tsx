4bdfea2c197d59742ee00fded0815f91f38b90d9
dada-tech <david.olu.dada@gmail.com>
2023-07-22 23:44:54 -0600

    eslint plugin fixed and re-configured. minor code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Login from './pages/Login/Login'
-// import {GlobalStyle} from "./theme/theme";
 
 const App = createBrowserRouter([
     {
