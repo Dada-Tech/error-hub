3788e0dac907bc351220144db002e12ddfcdff26
davir <ddebrah@ymail.com>
2023-07-28 00:31:22 -0600

    Autocomplete Styling setup

--- src/pages/Home/autocompleteStyling.tsx
+++ src/pages/Home/autocompleteStyling.tsx
@@ -2,23 +2,23 @@ import { makeStyles } from "@material-ui/styles";
 
 const autocompleteStyling = makeStyles(() => ({
     root: {
-        borderRadius: "13px",
-        "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
-            // Default transform is "translate(14px, 20px) scale(1)""
-            // This lines up the label with the initial cursor position in the input
-            // after changing its padding-left.
-            transform: "translate(34px, 20px) scale(1);"
-        },
+        // "& .MuiInputLabel-outlined:not(.MuiInputLabel-shrink)": {
+        //     // Default transform is "translate(14px, 20px) scale(1)""
+        //     // This lines up the label with the initial cursor position in the input
+        //     // after changing its padding-left.
+        //     transform: "translate(34px, 20px) scale(1);"
+        // },
         "&.Mui-focused .MuiInputLabel-outlined": {
             color: "black",
             background: "#0089ED",
-            // filter: "brightness(85%)"
-        }
+        },
+          "&.MuiAutocomplete-hasPopupIcon.css-1h51icj-MuiAutocomplete-root .MuiOutlinedInput-root": {
+              borderRadius: "13px",
+          }
     },
     inputRoot: {
         color: "black",
         background: "white",
-        borderRadius: "13px",
         '&[class*="MuiOutlinedInput-root"] .MuiAutocomplete-input:first-child': {
             // Default left padding is 6px
             paddingLeft: 26
