004af068e06a9fd6f514de590a36b49b946128a6
davir <ddebrah@ymail.com>
2023-08-08 19:06:38 -0600

    minor changes

--- src/components/LogoandName/LogoandName.tsx
+++ src/components/LogoandName/LogoandName.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import classes from './LogoandName.module.scss';
+// import classes from './LogoandName.module.scss';
 import errohublogo from '../Images/errohublogo.png';
 
 function OuterDiv() {
     return (
-        <div className={classes.outerdiv}>
-            <img className={classes.errorhublogo} src={errohublogo} alt="errohublogo" />
-            <div className={classes.header}> Error-Hub </div>
+        <div className="w-[272px] h-[79px] relative left-[28px]">
+            <img className="left-0 top-[13px] absolute min-h-0 min-w-0 w-16 shrink-0" src={errohublogo} alt="errohublogo" />
+            <div className="left-[68px] top-[16px] absolute text-5xl font-['Saira_SemiCondensed'] font-semibold text-secondBlack ] w-[224px]">Error-Hub</div>
         </div>
     );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -29,19 +29,36 @@ function Home() {
                 <Container/>
                 <LogoandName/>
 
-                <p className={classes.typography}>
-                    welcome to
-                    <p className={classes.typography3}> errorhub </p>
-                </p>
-                <p className={classes.typography2}>
-                    An application in which users <br/>
-                    can submit errors found in E-books along <br/>
-                    with auto-filled metadata to correctly identify <br/>
-                    the book. These reports will be uploaded to <br/>
-                    a database.
-                </p>
+                <div className="relative left-[73px] w-[396px] h-[87px] flex-col justify-start items-start inline-flex">
+                    <div className={classes.typography}>
+                        <div className="text-white text-[34px] font-semibold">Welcome to <br/> </div>
+                        <div className="text-white text-2xl font-normal">Errorhub</div>
+                    </div>
 
-                <img className={classes.sallyimg} src={sallyImg} alt="sallyImg"/>
+                    <div className="self-stretch grow shrink basis-0 text-white text-[13px] font-light">
+                        <div>
+                            An application in which users <br/>
+                            can submit errors found in E-books along <br/>
+                            with auto-filled metadata to correctly identify <br/>
+                            the book. These reports will be uploaded to <br/>
+                            a database.
+                        </div>
+                    </div>
+
+                    <img className={classes.sallyimg} src={sallyImg} alt="sallyImg"/>
+                </div>
+
+                {/*<div className="w-[329px] h-[115px] flex-col justify-start items-start inline-flex">*/}
+                {/*    <div className="self-stretch grow shrink basis-0 text-white text-[13px] font-light">*/}
+                {/*        An application in which users <br/>*/}
+                {/*        can submit errors found in E-books along <br/>*/}
+                {/*        with auto-filled metadata to correctly identify <br/>*/}
+                {/*        the book. These reports will be uploaded to <br/>*/}
+                {/*        a database.*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+
+                {/*<img className={classes.sallyimg} src={sallyImg} alt="sallyImg"/>*/}
 
                 <Autocomplete
                     classes={muiStyling}
