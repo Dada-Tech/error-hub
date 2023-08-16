3d193973336f6c84e94c79d185d778b4d515e393
davir <ddebrah@ymail.com>
2023-08-16 15:59:48 -0600

    minor changes

--- src/components/Container/secondContainer.tsx
+++ src/components/Container/secondContainer.tsx
@@ -5,7 +5,7 @@ import TextField from '@mui/material/TextField';
 
 const MyContainer = () => {
     return (
-        <Container sx={{width: '635px'}} className="rounded-3xl m-32 mb-0 shadow-md hover:shadow-xl flex h-[779px] flex-col justify-center items-center flex-shrink-0 bg-white">
+        <Container sx={{width: '635px'}} className="rounded-3xl m-32 mb-0 shadow-md hover:shadow-xl flex h-[779px] flex-col justify-center flex-shrink-0 items-center bg-white">
             <div className="pl-4">
                 <div className="">
                     <div className="text-black text-[21px] pt-8">
@@ -14,40 +14,43 @@ const MyContainer = () => {
                         <span className="text-blue font-semibold">ErrorHub</span>
                     </div>
                     <div className="pt-4">
-                        <div className="text-black text-base font-normal capitalize">Book Title</div>
+                        <div className="pb-2 text-black text-base capitalize">Book Title</div>
                         <TextField
+                            sx={{ width: '526px'}}
                             placeholder="To Kill a Mockingbird"
                             variant="outlined"
                             hiddenLabel
                         />
                     </div>
                     <div className="pt-4">
-                        <div className="text-black text-base font-normal capitalize">Author(s)</div>
+                        <div className="pb-2 text-black text-base capitalize">Author(s)</div>
                         <TextField
+                            sx={{ width: '526px'}}
                             placeholder="Harper Lee"
                             variant="outlined"
                             hiddenLabel
                         />
                     </div>
                     <div className="pt-4">
-                        <div className="text-black text-base font-normal capitalize">ISBN</div>
+                        <div className="pb-2 text-black text-base capitalize">ISBN</div>
                         <TextField
+                            sx={{ width: '526px'}}
                             placeholder="9780062368683"
                             variant="outlined"
                             hiddenLabel
                         />
                     </div>
                     <div className="grid grid-cols-2 grid-rows-1 gap-4">
-                        <div className="pt-4">
-                            <div className="text-black text-base font-normal capitalize">Book Type</div>
+                        <div className="pt-8">
+                            <div className="pb-2 text-black text-base capitalize">Book Type</div>
                             <TextField
                                 placeholder="eg. kindle, mobi, pdf, ibook"
                                 variant="outlined"
                                 hiddenLabel
                             />
                         </div>
-                        <div className="pt-4">
-                            <div className="text-black text-base font-normal capitalize">Error Type</div>
+                        <div className="pt-8">
+                            <div className="pb-2 text-black text-base capitalize">Error Type</div>
                             <TextField
                                 placeholder="eg. Spelling, grammar, image"
                                 variant="outlined"
@@ -56,16 +59,16 @@ const MyContainer = () => {
                         </div>
                     </div>
                     <div className="grid grid-cols-2 grid-rows-1 gap-4">
-                        <div className="pt-4">
-                            <div className="text-black text-base font-normal capitalize">Chapter</div>
+                        <div className="pt-8">
+                            <div className="pb-2 text-black text-base capitalize">Chapter</div>
                             <TextField
                                 placeholder="Chapter"
                                 variant="outlined"
                                 hiddenLabel
                             />
                         </div>
-                        <div className="pt-4">
-                            <div className="text-black text-base font-normal capitalize">Page Number</div>
+                        <div className="pt-8">
+                            <div className="pb-2 text-black text-base capitalize">Page Number</div>
                             <TextField
                                 placeholder="Page Number"
                                 variant="outlined"
@@ -74,8 +77,8 @@ const MyContainer = () => {
                         </div>
                     </div>
                 </div>
-                <div className=" ">
-                    <Button variant="contained" color="primary" className="capitalize bg-lightBlue"> upload </Button>
+                <div className="pt-8 flex item-center justify-center">
+                    <Button variant="contained" sx={{ width: '526px'}} color="primary" className="h-[54px] bg-sky-500 rounded-[10px] shadow uppercase bg-lightBlue"> upload </Button>
                 </div>
             </div>
         </Container>
