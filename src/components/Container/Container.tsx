cfa3819fe86deb1b5c58328341a3e0b8dfb7d57e
davir <ddebrah@ymail.com>
2023-08-16 23:38:11 -0600

    minor changes

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -3,11 +3,7 @@ import { Box } from '@mui/material';
 
 function Container() {
   return (
-      <Box className="bg-white container max-w-full fixed top-[43%] bottom-0 px-4"/>
-
-      // </Box>
-
-    // <div className="container mx-auto px-4"> </div>
+      <Box className="bg-white container max-w-full fixed top-[43%] bottom-0 px-4 z-0"/>
   );
 }
 
