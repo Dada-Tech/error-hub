de11f54a0e6c7d2bf2dbe6fff2c17f4958b3d1b6
davir <ddebrah@ymail.com>
2023-08-18 15:54:36 -0600

    minor changes

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -3,15 +3,14 @@ import { GlobalStyle } from '../../theme/theme';
 import Container from '../../components/Container/Container';
 import LogoandName from "../../components/LogoandName/LogoandName";
 import SignUpForm from '../../components/Form/SignUpForm';
-import Title from "../../components/Title/Title";
-
+import Signuptitle from "../../components/Title/signupTitle";
 function SignUp() {
   return (
         <>
             <GlobalStyle />
             <Container />
             <LogoandName />
-            <Title/>
+            <Signuptitle/>
             <div className="fixed top-32 right-8 flex justify-end w-[1384px] pb-5">
                 <SignUpForm/>
             </div>
