8f5058fbcecf0c83823cca15b83750b076c642f1
davir <ddebrah@ymail.com>
2023-08-20 20:51:30 -0600

    remove comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-// import Home from './pages/Home/Home';
 import Errorpage from "./pages/Errorpage/Errorpage";
 
 const App = createBrowserRouter([
